0a917193ee02aacd76c7135ee0a4d327a8fe2a96
jianran.tfh <jianran.tfh@alibaba-inc.com>
2020-08-23 21:11:00 +0800

    init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    private static final Log logger = LogFactory.getLog(HelloController.class);
+
+    @RequestMapping("/hello.htm")
+    public @ResponseBody String hello() {
+        logger.error("XXX" + System.currentTimeMillis());
+        return "hello jianran";
+    }
+}
